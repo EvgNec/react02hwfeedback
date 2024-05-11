db93c95c135edc813ec321d678143b45a81b5d36
Evgeniy <evgnec@gmail.com>
2024-05-11 15:27:39 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Feedback from './Feedback/Feedback';
+
+
+export class App extends Component {
+  // значнен за замовчанням, якщо від пропсів нічого не прийде
+  static defaultProps = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  // поточне значення state
+  state = {
+    good: this.props.good,
+    neutral: this.props.neutral,
+    bad: this.props.bad,
+  };
+
+  render() {
+    // отримуємо значення зі state
+    const { good, neutral, bad } = this.state; 
+    return (
+    <div>
+      <Feedback
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
+      </div>
+    );
+  }
+}
